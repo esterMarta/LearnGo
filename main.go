313c5afafcc0853016e3765de0b35787e2655df3
Ester Tambunan <estertambunan@outlook.com>
2022-01-21 11:23:55 +0700

    mascot

--- main.go
+++ main.go
@@ -4,29 +4,8 @@ import (
 	"fmt"
 
 	"estermarta.com/go-learn-1/mascot"
-
-	"path"
 )
 
 func main() {
 	fmt.Println(mascot.BestMascot())
-
-	var speedx int
-
-	fmt.Println(speedx)
-
-	var dir, file string
-
-	dir, file = path.Split("css/main.css")
-
-	fmt.Println(dir, file)
-
-	speed := 100
-	force := 2.5
-
-	//speed = speed * int(force) //200
-	speed = int(float64(speed) * force) //250
-
-	fmt.Println(speed)
-
 }
